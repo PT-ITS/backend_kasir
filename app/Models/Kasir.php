004ebb998db83a7889a70732ba9bc0873cf57dee
raflijr <raflijr3101@gmail.com>
2025-06-16 10:49:43 +0700

    fix kasir

--- app/Models/Kasir.php
+++ app/Models/Kasir.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Kasir extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nama_kasir',
+        'hp_kasir',
+        'alamat_kasir',
+        'fk_id_toko',
+        'fk_id_user',
+    ];
 }
